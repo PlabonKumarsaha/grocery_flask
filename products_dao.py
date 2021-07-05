7435f276c82ff79061930c9c93e924f68f7fc74b
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-07-05 20:33:21 +0600

    delete of produce implmented

--- products_dao.py
+++ products_dao.py
@@ -23,17 +23,12 @@ def get_all_products(connection):
     # cnx.close()
     return response;
 
-def insert_new_product(connection, product):
-    cursor = connection.cursor()
-    query = ("INSERT INTO gs.produts(name,UOM_id,price_per_unit)"
-             "VALUES(%s,%s,%s)")
-    name = product['name'];
-    uom = product['UOM_id']
-    price = product['price_per_unit']
-    cursor.execute(query, (name, uom, price))
-    connection.commit()
 
-    return cursor.lastrowid
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM gs.produts WHERE product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
 
 
 def insert_new_product(connection, product):
@@ -53,7 +48,9 @@ if __name__ == '__main__':
     connection = get_sql_connection()
     # print(get_all_products(connection))
     print(insert_new_product(connection, {
-        'name': 'Potato',
+        'name': 'Carrrot',
         'UOM_id': '2',
-        'price_per_unit': '12',
+        'price_per_unit': '20',
     }))
+
+    print(delete_product(connection, 9));
